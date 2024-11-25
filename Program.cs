6efa8c89d3c23aa89e369bdb672fc94e5c402c55
Talita <talinunes@yahoo.com.br>
2024-11-25 17:09:22 -0300

    feat: Add seeding service

--- Program.cs
+++ Program.cs
@@ -8,36 +8,41 @@ namespace MyBookStore
     {
         public static void Main(string[] args)
         {
-			var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);
 
-			builder.Services.AddDbContext<MyBookStoreContext>(options =>
-			{
-				options.UseMySql(
-					builder
-						.Configuration
-						.GetConnectionString("MyBookStoreContext"),
-					ServerVersion
-						.AutoDetect(
-							builder
-								.Configuration
-								.GetConnectionString("MyBookStoreContext")
-						)
-				);
-			});
+            builder.Services.AddDbContext<MyBookStoreContext>(options =>
+            {
+                options.UseMySql(
+                    builder
+                        .Configuration
+                        .GetConnectionString("MyBookStoreContext"),
+                    ServerVersion
+                        .AutoDetect(
+                            builder
+                                .Configuration
+                                .GetConnectionString("MyBookStoreContext")
+                        )
+                );
+            });
 
             builder.Services.AddScoped<GenreService>();
+            builder.Services.AddScoped<SeedingService>();
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+            else
+            {
+                // Criamos um escopo de execução nos serviços, usamos o GetRequiredService para selecionar o serviço a ser executado e selecionamos o método Seed().
+                app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
             }
 
             app.UseHttpsRedirection();
